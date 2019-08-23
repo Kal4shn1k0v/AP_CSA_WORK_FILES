619bb66f0814470db960ef3def5a9e39e2caf901
Kal <danielzst20032@yandex.com>
2019-08-22 21:56:57 -0700

    Maven-Greenfoot Template
    Upload 1

---
+++ Greenfoot Template/src/main/java/projekt/actors/DemoActor.java
@@ -0,0 +1,13 @@
+package projekt.actors;
+
+import greenfoot.Actor;
+
+public class DemoActor extends Actor {
+    public DemoActor() {
+        getImage().scale(50,50);
+    }
+
+    public void act() {
+        //Insert Actions
+    }
+}
