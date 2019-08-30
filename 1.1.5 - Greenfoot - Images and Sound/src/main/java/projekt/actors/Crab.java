ffd79b8b91374831e55f97a7919b114aca666710
Kal <danielzst20032@yandex.com>
2019-08-30 11:28:03 -0700

    1.1.5
    Upload 2

--- 1.1.5 - Greenfoot - Images and Sound/src/main/java/projekt/actors/Crab.java
+++ 1.1.5 - Greenfoot - Images and Sound/src/main/java/projekt/actors/Crab.java
@@ -1,13 +1,34 @@
 package projekt.actors;
 
 import greenfoot.Actor;
+import greenfoot.Greenfoot;
+import greenfoot.GreenfootImage;
+
+import java.util.concurrent.TimeUnit;
 
 public class Crab extends Actor {
     public Crab() {
         getImage().scale(50,50);
     }
 
+    //GreenfootImage 1 = new GreenfootImage("crab.png");
+
+
     public void act() {
+        int infinity = 10;
+        while (infinity >  5) { /*
+            if (new GreenfootImage("crab.png").equals(getImage())){
+                setImage("crab2.png");
+            }  */
+
+            getImage().scale(50,50);
+            Greenfoot.delay(1);
+            getImage().scale(60,50);
+            Greenfoot.delay(1);
+        }
+
+            move(4);
+
         if (isTouching(Lobster.class)) {
             System.out.println("Collision!");
             getWorld().removeObject(this);
