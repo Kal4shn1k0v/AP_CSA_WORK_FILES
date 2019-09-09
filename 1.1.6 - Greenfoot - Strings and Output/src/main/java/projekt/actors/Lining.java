6d18e273b461db21c8ad81056e6f8dfd3f62b87f
Kal <danielzst20032@yandex.com>
2019-09-09 12:47:37 -0700

    1.1.6
    Upload 4

--- 1.1.6 - Greenfoot - Strings and Output/src/main/java/projekt/actors/Lining.java
+++ 1.1.6 - Greenfoot - Strings and Output/src/main/java/projekt/actors/Lining.java
@@ -4,5 +4,11 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Lining extends Actor {
     public void act() {
+        setLocation(getX()-2, getY());
+
+        if (getX() == 0) {
+            getWorld().removeObject(this);
+            getWorld().addObject(this,225, 360);
+        }
     }    
 }
