690d6beb68ac3b4c531eeed2135aae14a5e2d2d0
Kal <danielzst20032@yandex.com>
2019-09-17 11:02:40 -0700

    1.1.7
    Upload 1

---
+++ 1.1.7 - Greenfoot/src/main/java/projekt/DemoApp.java
@@ -0,0 +1,10 @@
+package projekt;
+
+import greenfoot.Greenfoot;
+import greenfoot.export.GreenfootScenarioApplication;
+
+public class DemoApp extends GreenfootScenarioApplication {
+    public static void main(String[] args) {
+        GreenfootScenarioApplication.launch(args);
+    }
+}
---
+++ 1.1.7 - Greenfoot/src/main/java/projekt/worlds/MyWorld.java
@@ -0,0 +1,23 @@
+package projekt.worlds;
+
+import greenfoot.World;
+
+public class MyWorld extends World {
+    //Global variables
+    public MyWorld() {
+        super(780, 360, 1);
+        prepare();
+    }
+
+    public void act() {
+
+    }
+
+    private void prepare() {
+        addObjects();
+    }
+
+    private void addObjects() {
+
+    }
+}
