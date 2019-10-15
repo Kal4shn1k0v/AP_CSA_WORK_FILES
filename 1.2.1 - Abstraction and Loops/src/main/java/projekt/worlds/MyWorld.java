1c1019b307a38edd2f47079e455b2d0c5854acb6
Kal <danielzst20032@yandex.com>
2019-10-15 11:01:49 -0700

    1.2.1
    Upload 1

---
+++ 1.2.1 - Abstraction and Loops/src/main/java/projekt/worlds/MyWorld.java
@@ -0,0 +1,23 @@
+package projekt.worlds;
+
+import greenfoot.Greenfoot;
+import greenfoot.World;
+//import projekt.actors.*;
+
+/**
+ *
+ */
+public class MyWorld extends World {
+
+    public MyWorld() {
+        super(1000, 1000, 1);
+        prepare();
+    }
+
+    private void prepare() {
+        addObjects();
+    }
+
+    private void addObjects() {
+    }
+}
---
+++ Trial apps/src/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void main(String[] args) {
+        double fact1 = (double )1 / 2;
+
+        double fact2 = 3 * 4;
+
+        double product = fact1 * fact2;
+
+        System.out.println(product);
+
+    }
+}
