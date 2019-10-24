64f27edb4925264ab2a2dc66a3634eed4d26d186
Kal <danielzst20032@yandex.com>
2019-10-24 12:31:42 -0700

    1.2.2
    Upload 1

---
+++ 1.2.2 - Bubbles/src/main/java/projekt/DemoApp.java
@@ -0,0 +1,15 @@
+package projekt;
+
+import greenfoot.Greenfoot;
+import greenfoot.export.GreenfootScenarioApplication;
+
+import java.util.Arrays;
+
+/**
+ * @author Lukas Fülling (lukas@k40s.net)
+ */
+public class DemoApp extends GreenfootScenarioApplication {
+    public static void main(String[] args) {
+        GreenfootScenarioApplication.launch(args);
+    }
+}
---
+++ 1.2.2 - Bubbles/src/main/java/projekt/worlds/MyWorld.java
@@ -0,0 +1,16 @@
+package projekt.worlds;
+
+import greenfoot.Greenfoot;
+import greenfoot.GreenfootImage;
+import greenfoot.World;
+
+import java.util.Arrays;
+
+public class MyWorld extends World {
+    public MyWorld()
+    {
+        super(900, 600, 1);
+        getBackground().setColor(Color.BLACK);
+        getBackground().fill();
+    }
+}
